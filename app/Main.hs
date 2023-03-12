4cbb95f0fb58def21ac612eaeac4845b087eff99
miracoly <68049792+miracoly@users.noreply.github.com>
2023-03-13 00:07:22 +0100

    add chapter 3.3 & 3.4

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,14 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = putStrLn myHtml
+main = putStrLn $ render myHtml
 
-myHtml :: String
+myHtml :: Html
 myHtml =
-  makeHtml
-    "My title" 
-    (h1_ "My headline" <> p_ "Let's learn about haskell")
+  html_
+    "My title" $
+    append_
+      (h1_ "My headline") $
+      append_
+        (p_ "Let's learn about haskell")
+        (p_ "Paragraph 2")
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,27 +2,35 @@ module Lib
   ( module Lib,
   )
 where
+  
+newtype Html = Html String
+newtype Structure = Structure String
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_ $ head_ (title_ title) <> body_ content
+html_ :: String -> Structure -> Html
+html_ title content =
+  Html $
+    el "html" $
+      el "head" (el "title" title) <>
+      el "body" (getString content)
 
-html_ :: String -> String
-html_ = el "html"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-body_ :: String -> String
-body_ = el "body"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure str1) (Structure str2) = Structure (str1 <> str2)
+
+getString :: Structure -> String
+getString struct =
+  case struct of
+    Structure str -> str
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
