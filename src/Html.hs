01c05dd0a75ddb45c0986e70ce7eea8fe6b9b368
miracoly <68049792+miracoly@users.noreply.github.com>
2023-03-13 22:02:22 +0100

    add chapter 3.6

--- src/Html.hs
+++ src/Html.hs
@@ -3,6 +3,7 @@
 module Html
   ( Html,
     Structure,
+    Title,
     html_,
     p_,
     h1_,
@@ -17,21 +18,35 @@ newtype Structure = Structure String
 
 type Title = String
 
+-- * EDSL
+
 html_ :: Title -> Structure -> Html
 html_ title content =
   Html $
     el "html" $
-      el "head" (el "title" title)
+      el "head" (el "title" ( escape title))
         <> el "body" (getString content)
 
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . el "p" . escape
 
 h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+h1_ = Structure . el "h1" . escape
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+escape :: String -> String
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concatMap escapeChar
 
 append_ :: Structure -> Structure -> Structure
 append_ (Structure str1) (Structure str2) = Structure (str1 <> str2)
