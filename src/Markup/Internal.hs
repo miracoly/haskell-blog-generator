815cc9eb836ed2eb6821237b9a5f0d08b7ec4e5f
miracoly <68049792+miracoly@users.noreply.github.com>
2023-03-14 22:56:03 +0100

    add chapter 4.2

--- src/Markup/Internal.hs
+++ src/Markup/Internal.hs
@@ -11,3 +11,19 @@ data Structure
   | OrderedList [String]
   | CodeBlock [String]
   deriving (Show)
+  
+parse :: String -> Document
+parse = parseLines [] . lines
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let paragraph = Paragraph (unlines (reverse currentParagraph))
+   in case txts of
+        [] -> [paragraph]
+        ln : lns ->
+          if trim ln == ""
+            then paragraph : parseLines [] lns
+            else parseLines (ln : currentParagraph) lns
+
+trim :: String -> String
+trim = unwords . words
